97800401490d35fc6bdd21dedc9bff602fdb28f5
최서연 <choe.seoyeon@mintech.kr>
2021-11-29 16:28:03 +0900

    feat: 프로젝트 파일 추가

---
+++ Bookish/Bookish/BookishApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookishApp.swift
+//  Bookish
+//
+//  Created by 최서연 on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct BookishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
